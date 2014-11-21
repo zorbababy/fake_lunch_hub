cffbcd1ce36012292034200abb3c189fd93da663
Tom Enden <tome@wix.com>
2014-11-21 15:36:57 +0200

    first commit

---
+++ fake_lunch_hub/config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+FakeLunchHub::Application.config.secret_token = '6b67409f7ae7b091c41dcbd64cd2b78a020d5cf40bc528d6d5056ada1c948a0a1793606b4fa7806b6ea6f75e1055bc564b58bc9a95a5ef9c55d7d4c354f1a274'
